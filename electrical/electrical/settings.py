758000498cce2cda9bc07df7e2ace79b72bca7da
ishwer dutt <96070571+ishwerdutt@users.noreply.github.com>
2023-03-15 23:16:15 +0530

    Update settings.py

--- electrical/electrical/settings.py
+++ electrical/electrical/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,6 +122,10 @@ USE_I18N = True
 
 USE_TZ = True
 CSRF_COOKIE_SECURE  = True
+CSRF_TRUSTED_ORIGINS = [
+    'https://electrical'
+]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
