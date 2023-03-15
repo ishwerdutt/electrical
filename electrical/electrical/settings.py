62c8a3b74739c3803a7082bae39cb71765e750e8
ishwer dutt <96070571+ishwerdutt@users.noreply.github.com>
2023-03-15 22:26:31 +0530

    updated

--- electrical/electrical/settings.py
+++ electrical/electrical/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ergwijf@1lufzv$lc$=)g5o&5)i=40$pzj%6rb(yf0gknox5)6"
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "django-insecure-ergwijf@1lufzv$lc$=)g5o&5)i=40$pzj%6rb(yf0gknox5)6")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -75,9 +76,15 @@ WSGI_APPLICATION = "electrical.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+if os.environ.get('DATABASE_URL'): 
 
-DATABASES = {
-    "default": {
+    DATABASES = {
+        'default' : 'dj_database_url.config(conn_max_age=500)'
+    }
+
+else:
+    DATABASES = {
+        "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
@@ -114,12 +121,13 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
+CSRF_COOKIE_SECURE  = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR/'staticfiles'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
