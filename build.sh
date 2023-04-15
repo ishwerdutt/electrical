3bfa82e9238071d24d760bb962e4ec54408b60be
ishwer dutt <96070571+ishwerdutt@users.noreply.github.com>
2023-04-15 11:18:47 +0530

    moved req file

--- build.sh
+++ build.sh
@@ -2,11 +2,7 @@
 # exit on error
 set -o errexit
 
-<<<<<<< HEAD
-pip install -r ./electrical/requirements.txt
-=======
 pip install -r requirements.txt
->>>>>>> 9f9cc44dc4ff56fc553e90d02f0e0fc27dcd4cd6
 
 python manage.py collectstatic --no-input
 python manage.py migrate
